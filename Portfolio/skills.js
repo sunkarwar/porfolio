2be3c6d26f6130967c6830f0b15161641ab7610e
sunkarwar <sunkarwar@gmail.com>
2024-02-14 08:05:27 -0500

    initial commit

---
+++ Portfolio/skills.js
@@ -0,0 +1,7 @@
+function onSubmitMessage() {
+    const name = document.getElementById("name").value;
+    const message = document.getElementById("message").value;
+    const div = document.getElementById("div_recommendations");
+    div.style.display= "block";
+    div.innerHTML += `<div id="div_msg"> <p>"${message}"` + (name ? ` - ${name} </p></div>` :  `</p></div>`);
+}
