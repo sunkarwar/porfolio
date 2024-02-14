697b163d6b8255573f4bb23fa426bfd0df791e66
sunkarwar <sunkarwar@gmail.com>
2024-02-14 09:43:20 -0500

    final

--- Portfolio/skills.js
+++ Portfolio/skills.js
@@ -2,7 +2,16 @@ function onSubmitMessage() {
     const name = document.getElementById("name").value;
     const message = document.getElementById("message").value;
     const div = document.getElementById("div_recommendations");
-    div.style.display= "block";
+    document.getElementById("popUp").style.display="block";
     div.innerHTML += `<div id="div_msg"> <p>"${message}"` + (name ? ` - ${name} </p></div>` :  `</p></div>`);
-    alert("Thank you for submitting a recommendation!");
+   
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function turnOff() {
+    document.getElementById("popUp").style.display="none";
 }
